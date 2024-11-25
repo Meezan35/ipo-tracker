f8ff879941febf34537de242a5eb28876f9181f5
Meezan Shaikh <heymeezan@gmail.com>
2024-11-25 14:01:21 +0530

    added-mock-json-data

--- src/pages/IpoList.js
+++ src/pages/IpoList.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import ipoData from "../mockData/ipos.json"
 
 const IpoList = () => {
+    const [ipos, setIpos] = useState([ipoData]);
   return (
-    <div>IpoList Hello</div>
+    <div></div>
     
   )
 }
